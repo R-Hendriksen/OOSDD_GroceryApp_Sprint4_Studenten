4811a50017038878356d68acfa7843a5bf5b011f
R-Hendriksen <s1207464@student.windesheim.nl>
2025-10-03 14:41:57 +0200

    toevoegen van enum Role en deze toepassen bij de Client en ClientRepository

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,14 +1,23 @@
-﻿
+﻿using Grocery.Core.Models;
+using System.Globalization;
+
 namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public Role Role { get; set; }
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
-            EmailAddress=emailAddress;
-            Password=password;
+            EmailAddress = emailAddress;
+            Password = password;
+            Role = Role.None;
+        }
+
+        public Client(int id, string name, string emailAddress, string password, Role role) : this(id, name, emailAddress, password)
+        {
+            Role = role;
         }
     }
 }
